a9e56f463455c97f496d9f3b00b28c6bd6697b05
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2021-04-24 16:29:39 +0200

    move chains code into applyChains and start generic applyDamage

--- src/main/java/chronosacaria/mcdw/mixin/enchantments/LivingEntityMixin.java
+++ src/main/java/chronosacaria/mcdw/mixin/enchantments/LivingEntityMixin.java
@@ -23,6 +23,7 @@ import net.minecraft.item.Items;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -30,16 +31,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin {
 
-	@Inject(method = "applyDamage(Lnet/minecraft/entity/damage/DamageSource;F)V", at = @At("HEAD"))
-	public void applyChainsEnchantment(DamageSource source, float amount, CallbackInfo info) {
-
+	@Unique
+	private void applyChains(DamageSource source, float amount, LivingEntity user) {
 		if (source.getSource() instanceof ArrowEntity) {
 			return;
 		}
-		if(!(source.getAttacker() instanceof PlayerEntity)) return;
 
-
-		LivingEntity user = (LivingEntity) source.getAttacker();
 		LivingEntity target = (LivingEntity) (Object) this;
 
 		if (source.getSource() instanceof LivingEntity && !source.isProjectile()) {
@@ -48,10 +45,9 @@ public class LivingEntityMixin {
 				if (user != null) {
 					mainHandStack = user.getMainHandStack();
 				}
-				boolean uniqueWeaponFlag = false;
 				if (McdwEnchantsConfig.getValue("chains")) {
 
-					if (mainHandStack != null && (EnchantmentHelper.getLevel(EnchantsRegistry.CHAINS, mainHandStack) >= 1 )) {
+					if (mainHandStack != null && (EnchantmentHelper.getLevel(EnchantsRegistry.CHAINS, mainHandStack) >= 1)) {
 						int level = EnchantmentHelper.getLevel(EnchantsRegistry.CHAINS, mainHandStack);
 
 						float chance = user.getRandom().nextFloat();
@@ -65,6 +61,16 @@ public class LivingEntityMixin {
 					}
 				}
 			}
+		}
+	}
+
+	@Inject(method = "applyDamage(Lnet/minecraft/entity/damage/DamageSource;F)V", at = @At("HEAD"))
+	public void applyDamage(DamageSource source, float amount, CallbackInfo info) {
+		if(!(source.getAttacker() instanceof PlayerEntity)) return;
+		LivingEntity user = (LivingEntity) source.getAttacker();
+
+		if (McdwEnchantsConfig.getValue("chains")) {
+			applyChains(source, amount, user);
 		}
 	}
 
